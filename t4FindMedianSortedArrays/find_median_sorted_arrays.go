5c2ee0d46370bf42cb8bfe59fb0b708852c521e2
swordman-sm <aevlp@aevlp.vip>
2021-12-21 01:03:45 +0800

    findMedianSortedArrays

---
+++ t4FindMedianSortedArrays/find_median_sorted_arrays.go
@@ -0,0 +1,71 @@
+package t4FindMedianSortedArrays
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	//设nums1的长度小于nums2的长度
+	if len(nums1) > len(nums2) {
+		return findMedianSortedArrays(nums2, nums1)
+	}
+	//设一个指针从nums1左侧移动
+	//nums1长度小于等于nums2的长度处理方案
+	//最小移动开始位置
+	low := 0
+	//最大移动结束位置
+	high := len(nums1)
+	//数组连接在一起时的中间位置index
+	k := (len(nums1) + len(nums2) + 1) >> 1
+	//初始化nums1数组的中间位置
+	nums1Mid := 0
+	//初始化nums2 数组的中间位置
+	nums2Mid := 0
+	for low <= high {
+		//分界线右侧是mid,分界线左侧是mid-1
+		nums1Mid = low + (high-low)>>1
+		nums2Mid = k - nums1Mid
+		//nums1中的分界线划多了要向左边回移
+		if nums1Mid > 0 && nums1[nums1Mid-1] > nums2[nums2Mid] {
+			high = nums1Mid - 1
+		} else if nums1Mid != len(nums1) && nums1[nums1Mid] < nums2[nums2Mid-1] {
+			//nums1中的分界线划少了,要向右边移动
+			low = nums1Mid + 1
+		} else {
+			//找到合适的就退出
+			break
+		}
+	}
+	midLeft := 0
+	midRight := 0
+	if nums1Mid == 0 {
+		midLeft = nums2[nums2Mid-1]
+	} else if nums2Mid == 0 {
+		midLeft = nums1[nums1Mid-1]
+	} else {
+		midLeft = max(nums1[nums1Mid-1], nums2[nums2Mid-1])
+	}
+
+	if (len(nums1)+len(nums2))&1 == 1 {
+		return float64(midLeft)
+	}
+
+	if nums1Mid == len(nums1) {
+		midRight = nums2[nums2Mid]
+	} else if nums2Mid == len(nums2) {
+		midRight = nums1[nums1Mid]
+	} else {
+		midRight = min(nums1[nums1Mid], nums2[nums2Mid])
+	}
+	return float64(midLeft+midRight) / 2
+}
+
+func max(m int, n int) int {
+	if m >= n {
+		return m
+	}
+	return n
+}
+
+func min(m int, n int) int {
+	if m <= n {
+		return m
+	}
+	return n
+}
